cf952d721da2b270da09eabaaa160e6b9353cd94
Piyal Darshana <tenamax@gmail.com>
2018-11-08 19:38:19 +0530

    Update handler.ts

--- handler.ts
+++ handler.ts
@@ -14,7 +14,7 @@ export const hello = async (event, context, cb) => {
 }
 
 export const register = async (event, context, cb) => {
-  const CU = new CognitoUserService("tenamax@gmail.com", "123123");
+  const CU = new CognitoUserService("sdsdsd@gmail.com", "123123");
   return CU.signUp().then(response => {
     return cb(null,
       {
@@ -63,7 +63,7 @@ export const register = async (event, context, cb) => {
 }
 
 export const login = async (event, context, cb) => {
-  const CU = new CognitoUserService("tenamax@gmail.com", "123123");
+  const CU = new CognitoUserService("sdsddsd@gmail.com", "123123");
   return CU.login().then(response => {
     return cb(null,
       {
@@ -113,6 +113,7 @@ export const login = async (event, context, cb) => {
 
 export const verify = (event, context, callback) => {
 
+  
   event.response.autoConfirmUser = true;
 
   if (event.request.userAttributes.hasOwnProperty("email")) {
